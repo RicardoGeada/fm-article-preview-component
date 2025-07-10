efc48c0c70de7d35269c94f73c5bce6c7eb5845b
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2025-07-10 15:21:25 +0200

    a11y: add key listener for Esc key, add aria attributes

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-document.getElementById("shareButton").addEventListener("click", (toggleShareToast));
+const shareButton = document.getElementById("shareButton");
+const toast = document.getElementById("toast");
+
+shareButton.addEventListener("click", (toggleShareToast));
 
 /**
  * Toggles the visibility of the share toast and activate state of the share button. 
- * 
- * @param {MouseEvent} e - The click event trigerred by the share button.
  */
-function toggleShareToast(e) {
-    const button = e.currentTarget;
-    const toast = document.getElementById("toast");
-
-    button.classList.toggle("card__share-button--active");
-    toast.classList.toggle("toast--active")
+function toggleShareToast() {
+    shareButton.classList.toggle("card__share-button--active");
+    const isActive = toast.classList.toggle("toast--active");
+    toast.setAttribute("aria-hidden", !isActive);
+    shareButton.setAttribute("aria-expanded", isActive);
 }
+
+/**
+ * Keylistener for 'Esc' key to close the toast.
+ */
+document.addEventListener("keydown", (e) => {
+    const toast = document.getElementById("toast");
+    if (e.key === "Escape" && toast.classList.contains("toast--active")) {
+        toggleShareToast();
+        shareButton.focus();
+    };
+})
